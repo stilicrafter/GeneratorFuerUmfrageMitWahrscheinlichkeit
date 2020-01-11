a62cf597527ee4c0065e5f90ea90411717ff8e99
stilicrafter <stilicrafter@gmail.com>
2020-01-11 11:26:33 +0100

    Für weitere Inet Provider eingefügt

--- src/generator.java
+++ src/generator.java
@@ -8,12 +8,27 @@ public class generator {
 		Random rand = new Random();
 
 		for (int i = 0; i < 30; i++) {
-			int a = rand.nextInt(50);
+			int a = rand.nextInt(100);
 
-			if (a > 0 && a <= 10)
-				System.out.print("a");
-			if (a > 11 && a <= 50)
-				System.out.print("b");
+			if (a > 0 && a <= 30)
+				System.out.print("Maingau");
+			if (a > 31 && a <= 50)
+				System.out.print("T-Online oder Deutsche Telekom");
+			if (a > 51 && a <= 55)
+				System.out.print("EWE");
+			if (a > 56 && a <= 75)
+					System.out.print("1&1 / GMX / WEB oder UnitedInternet");
+			if (a > 76 && a <= 85)
+					System.out.print("Vodafone (Per DSL)");
+			if (a > 86 && a <= 90)
+					System.out.print("Telefonica oder (O2/E-Plus)");
+			if (a > 91 && a <= 97)
+					System.out.print("EVM");
+			if (a > 98 && a <= 101)
+					System.out.print("Sonstige");
+			//Trennzeichen
+			System.out.print(";");
+			
 			
 			// Teil 1
 
