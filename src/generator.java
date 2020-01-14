ac1deec28a0ca4afa00903d497615434ddd6e458
stilicrafter <stilicrafter@gmail.com>
2020-01-14 09:54:42 +0100

    Erweitert

--- src/generator.java
+++ src/generator.java
@@ -6,34 +6,70 @@ public class generator {
 		// TODO Auto-generated method stub
 
 		Random rand = new Random();
+		int b = 0;
 
 		for (int i = 0; i < 30; i++) {
 			int a = rand.nextInt(100);
 
-			if (a > 0 && a <= 30)
+			if (a >= 0 && a <= 30)
 				System.out.print("Maingau");
-			if (a > 31 && a <= 50)
+			if (a >= 31 && a <= 50)
 				System.out.print("T-Online oder Deutsche Telekom");
-			if (a > 51 && a <= 55)
+			if (a >= 51 && a <= 55)
 				System.out.print("EWE");
-			if (a > 56 && a <= 75)
-					System.out.print("1&1 / GMX / WEB oder UnitedInternet");
-			if (a > 76 && a <= 85)
-					System.out.print("Vodafone (Per DSL)");
-			if (a > 86 && a <= 90)
-					System.out.print("Telefonica oder (O2/E-Plus)");
-			if (a > 91 && a <= 97)
-					System.out.print("EVM");
-			if (a > 98 && a <= 101)
-					System.out.print("Sonstige");
-			//Trennzeichen
+			if (a >= 56 && a <= 75)
+				System.out.print("1&1 / GMX / WEB oder UnitedInternet");
+			if (a >= 76 && a <= 85)
+				System.out.print("Vodafone (Per DSL)");
+			if (a >= 86 && a <= 90)
+				System.out.print("Telefonica oder (O2/E-Plus)");
+			if (a >= 91 && a <= 97)
+				System.out.print("EVM");
+			if (a >= 98 && a <= 101)
+				System.out.print("Sonstige");
+			// Trennzeichen
+			System.out.print(";");
+
+			// Teil 1
+
+			b = rand.nextInt(100);
+			if (b >= 0 && b <= 20)
+				System.out.print("1");
+			if (b >= 21 && b <= 56)
+				System.out.print("2");
+			if (b >= 57 && b <= 79)
+				System.out.print("3");
+			if (b >= 80 && b <= 88)
+				System.out.print("4");
+			if (b >= 89 && b <= 94)
+				System.out.print("5");
+			if (b >= 95 && b <= 99)
+				System.out.print("3");
+			if (b <= 100)
+				System.out.print("1");
+			
+			
+
+			
 			System.out.print(";");
 			
 			
-			// Teil 1
+			
+			
+			
+			
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			//System.out.println(a +"   "  + b);
+			// Teil 2
 
-			System.out.println(";");
-			//System.out.println(" ");
+			//System.out.println(";");
+			// System.out.println(" ");
 
 		}
 	}
