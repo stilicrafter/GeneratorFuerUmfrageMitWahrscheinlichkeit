6ae7db399b5402a89a411b7920bbdbf4fadfeab9
stilicrafter <stilicrafter@gmail.com>
2020-01-23 06:05:25 +0100

    Erweitert

--- src/generator.java
+++ src/generator.java
@@ -43,15 +43,38 @@ public class generator {
 				System.out.print("4");
 			if (b >= 89 && b <= 94)
 				System.out.print("5");
-			if (b >= 95 && b <= 99)
+			if (b >= 95 && b <= 101)
 				System.out.print("3");
-			if (b <= 100)
-				System.out.print("1");
+			//if (b <= 100)
+			//	System.out.print("1");
 			
 			
 
 			
 			System.out.print(";");
+			
+			
+	for (int j = 0; j < 16; j++) {
+		
+			
+			int c = rand.nextInt(100);
+			
+			if (c >= 0 && c <= 20)
+				System.out.print("Sehr oft");
+			if (c >= 21 && c <= 56)
+				System.out.print("Oft");
+			if (c >= 57 && c <= 79)
+				System.out.print("Manchmal");
+			if (c >= 80 && c <= 88)
+				System.out.print("Nie");
+			if (c >= 89 && c <= 94)
+				System.out.print("Sehr Oft");
+			if (c >= 95 && c <= 101)
+				System.out.print("Manchmal");
+			
+			System.out.print(";");
+			
+			}
 			
 			
 			
@@ -64,7 +87,7 @@ public class generator {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+			System.out.println(" ");
 			//System.out.println(a +"   "  + b);
 			// Teil 2
 
